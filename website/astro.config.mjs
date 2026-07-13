8fd0296ee7bcbb82a744438cd75fc5cc62dbde98
Rg <daimaldd@gmail.com>
2026-07-13 22:30:03 +0800

    allow website acess

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,4 +4,9 @@ export default defineConfig({
   site: "https://tact.0x81.hk",
   output: "static",
   trailingSlash: "never",
+  vite: {
+    preview: {
+      allowedHosts: ["website", "localhost", "127.0.0.1"],
+    },
+  },
 });
